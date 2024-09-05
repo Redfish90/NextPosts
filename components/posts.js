d18fb3f49fc9e4dc63cf548c475072bdd60f9030
crentifis <redifischris@gmail.com>
2024-09-05 16:08:41 +0300

    Replace img with <Image/>

--- components/posts.js
+++ components/posts.js
@@ -1,16 +1,24 @@
 "use client";
 
 import { useOptimistic } from 'react';
+import Image from 'next/image'
 
 import { formatDate } from '@/lib/format';
 import LikeButton from './like-icon';
 import { togglePostLikeStatus } from '@/actions/posts';
 
+function imageLoader(config) {
+    const SPLIT_WORD = 'upload/'
+    const [uploadStart, uploadEnd] = config.src.split(SPLIT_WORD)
+    const transformations = `w_200,q_${config.quality}`;
+    return `${uploadStart}${SPLIT_WORD}${transformations}/${uploadEnd}`
+}
+
 function Post({ post, action }) {
     return (
         <article className="post">
             <div className="post-image">
-                <img src={post.image} alt={post.title} />
+                <Image loader={imageLoader} src={post.image} fill alt={post.title} quality={50}/>
             </div>
             <div className="post-content">
                 <header>
